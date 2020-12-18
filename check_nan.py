9b18fe294b5a29c8f42fe911b9cf0003a313148a
jame jaidee <ekawit_jaidee@cmu.ac.th>
2020-12-19 00:58:05 +0700

    trainflasedata

---
+++ check_nan.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = pd.read_csv('train.csv',index_col=0)
+
+data.loc[data['Signal'] == 'wait ot hold', 'Signal'] = 'wait or hold'
+data.loc[data['Signal'] == 'buy', 'Signal'] = 0
+data.loc[data['Signal'] == 'sell', 'Signal'] = 1 
+data.loc[data['Signal'] == 'wait or hold', 'Signal'] = 2 
+
+count = 0
+
+for index, row in data.iterrows():
+    if row['Signal'] != 0 and row['Signal'] != 1 and row['Signal'] != 2:
+        count+=1
+        print(index,row['Signal'])
+print(count)
