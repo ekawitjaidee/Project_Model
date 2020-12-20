c2d2792ae80cdfa7f4cc56e0b9626e1edffb0d9e
jame jaidee <ekawit_jaidee@cmu.ac.th>
2020-12-20 23:16:20 +0700

    checknan

--- check_nan.py
+++ check_nan.py
@@ -1,16 +1,35 @@
 import pandas as pd
+import glob
 
-data = pd.read_csv('test1.csv')
 
-data.loc[data['Signal'] == 'wait ot hold', 'Signal'] = 'wait or hold'
-data.loc[data['Signal'] == 'buy', 'Signal'] = 0
-data.loc[data['Signal'] == 'sell', 'Signal'] = 1 
-data.loc[data['Signal'] == 'wait or hold', 'Signal'] = 2 
+def Check_nan(fname):
+    data = pd.read_csv(fname)
 
-count = 0
+    data.loc[data['Signal'] == 'wait ot hold', 'Signal'] = 'wait or hold'
+    data.loc[data['Signal'] == 'buy', 'Signal'] = 0
+    data.loc[data['Signal'] == 'sell', 'Signal'] = 1 
+    data.loc[data['Signal'] == 'wait or hold', 'Signal'] = 2 
 
-for index, row in data.iterrows():
-    if row['Signal'] != 0 and row['Signal'] != 1 and row['Signal'] != 2:
-        count+=1
-        print(index,row['Signal'])
-print(count)
+    count = 0
+
+    for index, row in data.iterrows():
+        if row['Signal'] != 0 and row['Signal'] != 1 and row['Signal'] != 2:
+            count+=1
+            print(index,row['Signal'])
+    print(count)
+
+
+def Check_nan_eachfile(fname,s):
+    df = pd.read_csv(fname)
+    for index, row in df.iterrows():
+        if row['Signal'] != 'buy' and row['Signal'] != 'sell' and row['Signal'] != 'wait or hold':
+            print(index ,fname)
+            s.add(fname)
+
+        
+datalist = [f for f in glob.glob("GT/*.csv")]
+s = {"j"}
+for stname in datalist:
+    Check_nan_eachfile(stname,s)
+print('---------------------done-----------------')
+print(s)
